09d1571a92776a418ee4306ea75f1f4beb714fb9
wjdwl002 <wjdwl001@khu.ac.kr>
2022-10-11 14:24:31 +0900

    [Feat/Home] 홈페이지 스타일 조정 + 버튼 스타일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@/assets/styles/index.scss";
 import "@/assets/styles/index.css";
+import "@/assets/styles/index.scss";
 import App from "@/App";
 import { ThemeProvider } from "@/context/ThemeContext";
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import tw from "tailwind-styled-components";
 import { Button } from "@/components/styled/Buttons";
+import { useTheme } from "@/context/ThemeContext";
 
 const ImgURL = "https://embed.lottiefiles.com/animation/63487";
 
 const Home = () => {
+  const { themeColorset } = useTheme();
+
   return (
     <HomeDiv>
-      <TitleDiv>
+      <TitleDiv style={{ color: themeColorset.pointColor }}>
         ConCoder
         <img width="200" src="src/assets/img/coder.gif" />
       </TitleDiv>
-      <BtnContainer>
+      <BtnContainer style={{ color: themeColorset.textColor }}>
         <BtnDiv>
           새로운 방을 만들고싶다면 ..
           <button>CREATE A WORKSPACE</button>
@@ -34,7 +37,8 @@ const HomeDiv = tw.div`
 
 const TitleDiv = tw.div`
  text-[70px] font-bold py-[100px] px-[40px]
- flex gap-[20px] tracking-wider
+ flex gap-[20px] justify-center items-center
+ tracking-wider
 `;
 
 const BtnContainer = tw.div`
