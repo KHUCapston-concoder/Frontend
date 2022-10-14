e7def4634b0db731a3f5d3967438556e6ce1662a
wjdwl002 <wjdwl001@khu.ac.kr>
2022-10-15 04:35:18 +0900

    [Env] tailwind.config 에 daisyui custom theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-  theme: {
-    extend: {},
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          accent: "#00adb5",
+          neutral: "#1b2128",
+          "base-100": "#222831",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
-}
+};
