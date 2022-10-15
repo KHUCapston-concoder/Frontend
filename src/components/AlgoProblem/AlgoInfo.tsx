99fdc75da6f1527d20560384427566e9b409eed2
wjdwl002 <wjdwl001@khu.ac.kr>
2022-10-15 22:44:26 +0900

    [Feat/Algo] 검색버튼 클릭시 dummyData 추가(recoil 사용)

--- src/components/AlgoProblem/AlgoInfo.tsx
+++ src/components/AlgoProblem/AlgoInfo.tsx
@@ -1,47 +1,52 @@
 import { useTheme } from "@/context/ThemeContext";
-import React from "react";
+import { algoProbState } from "@/store/algoProbState";
+import React, { useState } from "react";
+import { useRecoilValue } from "recoil";
 import tw from "tailwind-styled-components";
 
 const AlgoInfo = () => {
-  const { themeColorset } = useTheme();
+  const algoProblem = useRecoilValue(algoProbState);
+  const [probIndex, setProbIndex] = useState(0);
 
-  const algoInfo = {
-    num: 25848,
-    title: "보석 도둑",
-    timeRestraint: "1초",
-    memoryRestraint: "256MB",
-    problemContent: `세계적인 도둑 상덕이는 보석점을 털기로 결심했다. \n
-      상덕이가 털 보석점에는 보석이 총 N개 있다. 각, 보석은 무게 Mi와 가격 Vi를 가지고 있다. 상덕이는 가방을 K개 가지고 있고, 각 가방에 담을 수 있는 최대 무게는 Ci이다. 가방에는 최대 한 개의 보석만 넣을 수 있다. \n
-      상덕이가 훔칠 수 있는 보석의 최대 가격을 구하는 프로그램을 작성하시오.`,
-    input: `첫째 줄에 N과 K가 주어진다. (1 ≤ N, K ≤ 300,000) \n
-        다음 N개 줄에는 각 보석의 정보 Mi와 Vi가 주어진다. (0 ≤ Mi, Vi ≤ 1,000,000) \n
-        다음 K개 줄에는 가방에 담을 수 있는 최대 무게 Ci가 주어진다. (1 ≤ Ci ≤ 100,000,000) \n
-        모든 숫자는 양의 정수이다.`,
-    output: `첫째 줄에 상덕이가 훔칠 수 있는 보석 가격의 합의 최댓값을 출력한다.`,
-  };
+  console.log(algoProblem);
 
   return (
     <MainDiv>
-      <MenuController>
-        <i className="fa-solid fa-angle-left"></i>
-        <TitleHolder>
-          {algoInfo.num} {algoInfo.title}
-        </TitleHolder>
-        <i className="fa-solid fa-angle-right"></i>
-      </MenuController>
-      <RestraintDiv>
-        시간 복잡도 <RestraintHolder>{algoInfo.timeRestraint}</RestraintHolder>
-      </RestraintDiv>
-      <RestraintDiv>
-        공간 복잡도{" "}
-        <RestraintHolder>{algoInfo.memoryRestraint}</RestraintHolder>
-      </RestraintDiv>
-      <TitleDiv>· 문제</TitleDiv>
-      <ContentP>{algoInfo.problemContent}</ContentP>
-      <TitleDiv>· 입력</TitleDiv>
-      <ContentP>{algoInfo.input}</ContentP>
-      <TitleDiv>· 출력</TitleDiv>
-      <ContentP>{algoInfo.output}</ContentP>
+      {algoProblem.length == 0 ? (
+        <TempDiv>
+          <i className="fa-solid fa-circle-exclamation"></i>
+          필터를 설정해서 <br /> 문제를 탐색해보세요 !
+        </TempDiv>
+      ) : (
+        <>
+          <MenuController>
+            <i className="fa-solid fa-angle-left"></i>
+            <TitleHolder>
+              {algoProblem.list[probIndex].num}{" "}
+              {algoProblem.list[probIndex].title}
+            </TitleHolder>
+            <i className="fa-solid fa-angle-right"></i>
+          </MenuController>
+          <RestraintDiv>
+            시간 복잡도{" "}
+            <RestraintHolder>
+              {algoProblem.list[probIndex].timeRestraint}
+            </RestraintHolder>
+          </RestraintDiv>
+          <RestraintDiv>
+            공간 복잡도{" "}
+            <RestraintHolder>
+              {algoProblem.list[probIndex].memoryRestraint}
+            </RestraintHolder>
+          </RestraintDiv>
+          <TitleDiv>· 문제</TitleDiv>
+          <ContentP>{algoProblem.list[probIndex].problemContent}</ContentP>
+          <TitleDiv>· 입력</TitleDiv>
+          <ContentP>{algoProblem.list[probIndex].input}</ContentP>
+          <TitleDiv>· 출력</TitleDiv>
+          <ContentP>{algoProblem.list[probIndex].output}</ContentP>{" "}
+        </>
+      )}
     </MainDiv>
   );
 };
@@ -51,6 +56,12 @@ export default AlgoInfo;
 const MainDiv = tw.div`
 w-full h-full overflow-y-scroll overflow-x-hidden
 px-[14px] py-[10px]
+`;
+
+const TempDiv = tw.div`
+w-full h-full
+flex flex-col gap-[10px] justify-center items-center
+text-sm text-center
 `;
 
 const MenuController = tw.div`
---
+++ src/components/_styled/Tab.tsx
@@ -0,0 +1,42 @@
+import React, { SetStateAction, Dispatch } from "react";
+
+interface PropType {
+  onClick: Dispatch<SetStateAction<string>>;
+  placeholder: string;
+  className: string;
+  label: string;
+}
+
+const SelectBox = ({
+  options,
+  setSelection,
+  placeholder = "Select an option",
+  className = "select select-xs select-ghost w-full max-w-xs",
+  label,
+}: PropType) => {
+  const onSelectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target);
+
+    setSelection(e.target.value);
+  };
+  return (
+    <div className="tabs w-full">
+      <a
+        id="0"
+        className={`tab tab-lifted ${tabNum == 0 && "tab-active"} w-1/2`}
+        onClick={onClickTab}
+      >
+        필터검색
+      </a>
+      <a
+        id="1"
+        className={`tab tab-lifted ${tabNum == 1 && "tab-active"} w-1/2`}
+        onClick={onClickTab}
+      >
+        번호검색
+      </a>
+    </div>
+  );
+};
+
+export default SelectBox;
