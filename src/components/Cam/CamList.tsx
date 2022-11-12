d591eb6921c6847f9300c4e9f6d4f7a6606ee452
Charmull <527wlals@gmail.com>
2022-11-13 04:01:12 +0900

    [Feat/Cam] cam on/off 버튼 추가

--- src/components/Cam/CamList.tsx
+++ src/components/Cam/CamList.tsx
@@ -4,6 +4,25 @@ import { Button } from "../_styled/Buttons";
 const CamList = () => {
   let myStream: MediaStream;
   const myCam = useRef<HTMLVideoElement | undefined>(null);
+  const cameraBtn = useRef<HTMLButtonElement | undefined>(null);
+
+  // 현재 카메라 off 상태 담는 변수 (컴포넌트 재실행할 필요 없어서 state가 아닌 변수로 관리)
+  let cameraOff = false;
+
+  const camClickHandler = () => {
+    myStream.getVideoTracks().forEach((track) => {
+      track.enabled = !track.enabled;
+    });
+    if (cameraBtn.current != null) {
+      if (cameraOff) {
+        cameraBtn.current.innerText = "Cam Off";
+        cameraOff = false;
+      } else {
+        cameraBtn.current.innerText = "Cam On";
+        cameraOff = true;
+      }
+    }
+  };
 
   const getMedia = async () => {
     try {
@@ -28,6 +47,9 @@ const CamList = () => {
     <>
       <div>
         <video id="myCam" ref={myCam}></video>
+        <Button onClick={camClickHandler} ref={cameraBtn}>
+          Cam Off
+        </Button>
       </div>
     </>
   );
