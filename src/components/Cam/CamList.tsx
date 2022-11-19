9ee3ae919f531c587a35323a0bd909bb818eca03
Charmull <527wlals@gmail.com>
2022-11-19 17:58:27 +0900

    [Feat/Cam] WebRTC 코드 리펙토링
    코드 살짝 정리..

--- src/components/Cam/CamList.tsx
+++ src/components/Cam/CamList.tsx
@@ -22,26 +22,19 @@ const CamList = () => {
 
   // webRTC
   // 생성된 pc 정보를 가지는 map ({pc => "user02"} 형태)
-  const [pcMap, setPcMap] = useState<
-    Map<string, RTCPeerConnection | undefined> | undefined
-  >(new Map());
-  const pcs = useRef<Object | undefined>(new Object());
-  const [pcState, setPcState] = useState<Object | undefined>(new Object());
-  const setPcStateHandler = (memId, pc) => {
-    setPcState((prev: object) => {
+  const pcRef = useRef<Object | undefined>(new Object());
+  const [pcs, setPcs] = useState<Object | undefined>(new Object());
+  const setPcsHandler = (memId, pc) => {
+    setPcs((prev: object) => {
       return { ...prev, [memId]: pc };
     });
   };
-  const delPcStateHandler = (memId) => {
-    setPcState((prev: object) => {
+  const delPcsHandler = (memId) => {
+    setPcs((prev: object) => {
       delete prev[memId];
       return { ...prev };
     });
   };
-  // 생성된 dc 정보를 가지는 map
-  const [dcMap, setDcMap] = useState<
-    Map<string, RTCDataChannel | undefined> | undefined
-  >(new Map());
 
   // test용
   const [state, setState] = useState(0);
@@ -49,7 +42,7 @@ const CamList = () => {
     setState((prev) => {
       return !prev;
     });
-    console.log(pcMap);
+    console.log(pcRef);
   };
 
   // remoteCam Component Ref
@@ -63,6 +56,65 @@ const CamList = () => {
       makeMsg("exitPage", "exit", localId);
       console.log("exit!!!!!");
     });
+  };
+
+  // socket message
+  const memberEnterHandler = async (parsedMsg, pc) => {
+    console.log(
+      parsedMsg.from + " 멤버가" + parsedMsg.data + " 방에 입장했습니다!"
+    );
+    pc = remoteCamRef.current.makePeerConnection();
+    pcRef.current[parsedMsg.from] = pc;
+    setPcsHandler(parsedMsg.from, pc);
+    pc = remoteCamRef.current.setPeerConnection(pc, localId, parsedMsg.from);
+    const offer = await pc.createOffer();
+    pc.setLocalDescription(offer);
+    makeMsg("offer", offer, localId, parsedMsg.from);
+    console.log(parsedMsg.from + " 에게 offer 생성 후 보냄");
+  };
+
+  const memListHandler = (parsedMsg, pc) => {
+    console.log("기존 멤버 리스트입니다 " + parsedMsg.data);
+    for (let mem of parsedMsg.data) {
+      pc = remoteCamRef.current.makePeerConnection();
+      pcRef.current[mem] = pc;
+      setPcsHandler(mem, pc);
+      pc = remoteCamRef.current.setPeerConnection(pc, localId, mem);
+    }
+  };
+
+  const offerHandler = async (parsedMsg, pc) => {
+    console.log(parsedMsg.from + " 에게 offer 받음, offer : " + parsedMsg.data);
+    pc = pcRef.current[parsedMsg.from];
+    pc.setRemoteDescription(parsedMsg.data);
+    console.log(parsedMsg.from + "remoteDescription 설정");
+    const answer = await pc.createAnswer();
+    pc.setLocalDescription(answer);
+    console.log(parsedMsg.from + "locatDescription 설정");
+    makeMsg("answer", answer, localId, parsedMsg.from);
+    console.log(parsedMsg.from + "answer 생성 후 송신");
+  };
+
+  const answerHandler = (parsedMsg, pc) => {
+    console.log(
+      parsedMsg.from + " 에게 answer 받음, answer : " + parsedMsg.data
+    );
+    pc = pcRef.current[parsedMsg.from];
+    pc.setRemoteDescription(parsedMsg.data);
+    console.log("remoteDescription 설정");
+  };
+
+  const iceHandler = (parsedMsg, pc) => {
+    console.log("ice 수신");
+    pc = pcRef.current[parsedMsg.from];
+    pc.addIceCandidate(parsedMsg.data);
+    console.log("ice candidate 추가");
+  };
+
+  const memberExitHandler = (parsedMsg) => {
+    console.log(parsedMsg.from + " member 나감!!");
+    delete pcRef.current[parsedMsg.from];
+    delPcsHandler(parsedMsg.from);
   };
 
   useEffect(() => {
@@ -81,73 +133,22 @@ const CamList = () => {
           let pc: RTCPeerConnection | undefined;
           switch (parsedMsg.event) {
             case "memberEnter":
-              console.log(
-                parsedMsg.from +
-                  " 멤버가" +
-                  parsedMsg.data +
-                  " 방에 입장했습니다!"
-              );
-              pc = remoteCamRef.current.makePeerConnection();
-              pcs.current[parsedMsg.from] = pc;
-              pcMap?.set(parsedMsg.from, pc);
-              setPcStateHandler(parsedMsg.from, pc);
-              pc = remoteCamRef.current.setPeerConnection(
-                pc,
-                localId,
-                parsedMsg.from
-              );
-              const offer = await pc.createOffer();
-              pc.setLocalDescription(offer);
-              makeMsg("offer", offer, localId, parsedMsg.from);
-              console.log(parsedMsg.from + " 에게 offer 생성 후 보냄");
+              memberEnterHandler(parsedMsg, pc);
               break;
             case "memList":
-              console.log("기존 멤버 리스트입니다 " + parsedMsg.data);
-              for (let mem of parsedMsg.data) {
-                pc = remoteCamRef.current.makePeerConnection();
-                pcs.current[mem] = pc;
-                pcMap?.set(mem, pc);
-                setPcStateHandler(mem, pc);
-                pc = remoteCamRef.current.setPeerConnection(pc, localId, mem);
-              }
+              memListHandler(parsedMsg, pc);
               break;
             case "offer":
-              console.log(
-                parsedMsg.from + " 에게 offer 받음, offer : " + parsedMsg.data
-              );
-              pc = pcs.current[parsedMsg.from];
-              pc = pcMap?.get(parsedMsg.from);
-              // pcState[parsedMsg.from]
-              pc.setRemoteDescription(parsedMsg.data);
-              console.log(parsedMsg.from + "remoteDescription 설정");
-              const answer = await pc.createAnswer();
-              pc.setLocalDescription(answer);
-              console.log(parsedMsg.from + "locatDescription 설정");
-              makeMsg("answer", answer, localId, parsedMsg.from);
-              console.log(parsedMsg.from + "answer 생성 후 송신");
+              offerHandler(parsedMsg, pc);
               break;
             case "answer":
-              console.log(
-                parsedMsg.from + " 에게 answer 받음, answer : " + parsedMsg.data
-              );
-              pc = pcs.current[parsedMsg.from];
-              pc = pcMap?.get(parsedMsg.from);
-              pc.setRemoteDescription(parsedMsg.data);
-              console.log("remoteDescription 설정");
+              answerHandler(parsedMsg, pc);
               break;
             case "ice":
-              console.log("ice 수신");
-              pc = pcs.current[parsedMsg.from];
-              pc = pcMap?.get(parsedMsg.from);
-              pc.addIceCandidate(parsedMsg.data);
-              console.log("ice candidate 추가");
+              iceHandler(parsedMsg, pc);
               break;
             case "memberExit":
-              console.log(parsedMsg.from + " member 나감!!");
-              delete pcs.current[parsedMsg.from];
-              delPcStateHandler(parsedMsg.from);
-              pcMap?.delete(parsedMsg.from);
-
+              memberExitHandler(parsedMsg);
               break;
           }
         };
@@ -169,9 +170,7 @@ const CamList = () => {
           ws={ws}
           localStream={localStream}
           makeMsg={makeMsg}
-          pcMap={pcMap}
           pcs={pcs}
-          pcState={pcState}
           ref={remoteCamRef}
         />
       </div>
--- src/components/Cam/RemoteCam.tsx
+++ src/components/Cam/RemoteCam.tsx
@@ -7,122 +7,94 @@ import {
   useState,
 } from "react";
 
-const RemoteCam = forwardRef(
-  ({ ws, localStream, makeMsg, pcMap, pcs, pcState }, ref) => {
-    const memList = useRef<HTMLDivElement | undefined>(null);
-    const [remoteStreams, setRemoteStreams] = useState(new Object());
-    const delremoteStreamsHandler = (memId) => {
+const RemoteCam = forwardRef(({ ws, localStream, makeMsg, pcs }, ref) => {
+  const memList = useRef<HTMLDivElement | undefined>(null);
+  const [remoteStreams, setRemoteStreams] = useState(new Object());
+  const delremoteStreamsHandler = (memId) => {
+    setRemoteStreams((prev: object) => {
+      delete prev[memId];
+      return { ...prev };
+    });
+  };
+  let config = null;
+
+  useImperativeHandle(ref, () => ({
+    makePeerConnection,
+    setPeerConnection,
+  }));
+
+  // webRTC
+  const makePeerConnection = () => {
+    // RTC connection 객체 생성
+    const peerConnection = new RTCPeerConnection(config);
+
+    if (localStream != null) {
+      // 다른 user(원격 user)에게 전송될 트랙들에 myStream의 오디오, 비디오 트랙을 추가
+      localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+      });
+    }
+
+    return peerConnection;
+  };
+
+  const setPeerConnection = (pc, from, to) => {
+    // 다른 user(원격 user)에게 트랙을 받을 경우에 대해 eventListener 추가
+    pc.ontrack = (event) => {
       setRemoteStreams((prev: object) => {
-        delete prev[memId];
-        return { ...prev };
-      });
-    };
-    let config = null;
-
-    useImperativeHandle(ref, () => ({
-      makePeerConnection,
-      setPeerConnection,
-    }));
-
-    // webRTC
-    const makePeerConnection = () => {
-      // RTC connection 객체 생성
-      const peerConnection = new RTCPeerConnection(config);
-
-      if (localStream != null) {
-        // 다른 user(원격 user)에게 전송될 트랙들에 myStream의 오디오, 비디오 트랙을 추가
-        localStream.getTracks().forEach((track) => {
-          peerConnection.addTrack(track, localStream);
-        });
-      }
-
-      return peerConnection;
-    };
-
-    const setPeerConnection = (pc, from, to) => {
-      // 다른 user(원격 user)에게 트랙을 받을 경우에 대해 eventListener 추가
-      pc.ontrack = (event) => {
-        setRemoteStreams((prev: object) => {
-          return { ...prev, [to]: event.streams[0] };
-        });
-      };
-
-      pc.onicecandidate = (event) => {
-        console.log("ice candidate 얻음");
-        makeMsg("ice", event.candidate, from, to);
-      };
-
-      return pc;
-    };
-
-    const connectRemoteCam = () => {
-      while (memList.current?.hasChildNodes()) {
-        memList.current.removeChild(memList.current.firstChild);
-      }
-      Object.values(remoteStreams).forEach((stream) => {
-        let remoteCamTemp = document.createElement("video");
-        memList.current?.appendChild(remoteCamTemp);
-        remoteCamTemp.srcObject = stream;
-        let playPromise = remoteCamTemp.play();
-        if (playPromise != null) {
-          playPromise
-            .then((_) => {
-              // console.log(_);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }
+        return { ...prev, [to]: event.streams[0] };
       });
     };
 
-    useEffect(() => {
-      if (ws != null) {
-        connectRemoteCam();
+    pc.onicecandidate = (event) => {
+      console.log("ice candidate 얻음");
+      makeMsg("ice", event.candidate, from, to);
+    };
+
+    return pc;
+  };
+
+  const connectRemoteCam = () => {
+    while (memList.current?.hasChildNodes()) {
+      memList.current.removeChild(memList.current.firstChild);
+    }
+    Object.values(remoteStreams).forEach((stream) => {
+      let remoteCamTemp = document.createElement("video");
+      memList.current?.appendChild(remoteCamTemp);
+      remoteCamTemp.srcObject = stream;
+      let playPromise = remoteCamTemp.play();
+      if (playPromise != null) {
+        playPromise
+          .then((_) => {
+            // console.log(_);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
-    }, [remoteStreams]);
+    });
+  };
 
-    // useEffect(() => {
-    //   console.log("dkjaljfdljalf");
-    //   let streamKeys = Object.keys(remoteStreams);
-    //   console.log(streamKeys);
-    //   for (var streamKey in streamKeys) {
-    //     if (pcMap.has(streamKey) === false) {
-    //       setRemoteStreams((prev: object) => {
-    //         delete prev[streamKey];
-    //         console.log(prev);
-    //         return { ...prev };
-    //       });
-    //     }
-    //   }
-    // }, [pcMap]);
+  useEffect(() => {
+    if (ws != null) {
+      connectRemoteCam();
+    }
+  }, [remoteStreams]);
 
-    // useEffect(() => {
-    //   console.log("pcs!!!!!!!!");
-    //   let streamKeys = Object.keys(remoteStreams);
-    //   console.log(pcs.current);
-    //   for (var streamKey of streamKeys) {
-    //     if (pcs.current.hasOwn(streamKey) === false) {
-    //       delremoteStreamsHandler(streamKey);
-    //     }
-    //   }
-    // }, [pcs]);
-
-    useEffect(() => {
-      let streamKeys = Object.keys(remoteStreams);
-      for (const streamKey of streamKeys) {
-        if (Object.hasOwn(pcState, streamKey) === false) {
-          delremoteStreamsHandler(streamKey);
-        }
+  useEffect(() => {
+    let streamKeys = Object.keys(remoteStreams);
+    for (const streamKey of streamKeys) {
+      if (Object.hasOwn(pcs, streamKey) === false) {
+        delremoteStreamsHandler(streamKey);
       }
-    }, [pcState]);
+    }
+  }, [pcs]);
 
-    return (
-      <>
-        <div ref={memList}></div>
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <div ref={memList}></div>
+    </>
+  );
+});
 
 export default RemoteCam;
