f18490c0f8e1782fa10c37d337e3a17778de72db
wjdwl002 <wjdwl001@khu.ac.kr>
2022-12-01 11:19:03 +0900

    [Feat/Snapshot] 메모 기능 작업중

--- src/components/Snapshot/SnapshotInfo.tsx
+++ src/components/Snapshot/SnapshotInfo.tsx
@@ -1,9 +1,15 @@
-import { useGet } from "@/hooks/useHttp";
-import { ISnapshotDetail, ISnapshotInfo } from "@/interface/Snapshot";
-import { snapshotState } from "@/store/snapshotState";
-import React from "react";
+import { useDelete, useGet } from "@/hooks/useHttp";
+import { ISnapshotDetail, ISnapshotInfo } from "@/interface/ISnapshot";
+import {
+  snapshotListState,
+  snapshotState,
+  snapshotLengthState,
+} from "@/store/snapshotState";
+import React, { useState } from "react";
 import { useRecoilState } from "recoil";
 import tw from "tailwind-styled-components";
+import { IconButton } from "../_styled/Buttons";
+import InputBox from "../_styled/Input";
 
 interface PropType {
   data: ISnapshotInfo;
@@ -11,17 +17,39 @@ interface PropType {
 
 const SnapshotInfo = ({ data }: PropType) => {
   const [snapshotDetail, setSnapshotDetail] = useRecoilState(snapshotState);
-  const { sendRequest } = useGet(
+  const [snapshotList, setSnapshotList] = useRecoilState(snapshotListState);
+  const [editingMemo, setEditingMemo] = useState(false);
+  const [memo, setMemo] = useState("");
+  const [snapshotLength, setSnapshotLength] =
+    useRecoilState(snapshotLengthState);
+
+  const { sendRequest: sendGet } = useGet(
     { url: `/api/snapshots/${data.id}` },
     (data: ISnapshotDetail) => {
       setSnapshotDetail(() => data);
     }
   );
 
-  const onAddMemo = () => {};
+  const { sendRequest: sendDelete } = useDelete({
+    url: `/api/snapshots/${data.id}`,
+  });
+  const onDeleteSnapshot = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    sendDelete();
+    setSnapshotList(({ list }) => {
+      console.log(list);
+      list.filter((e: ISnapshotInfo) => e.id !== data.id);
+      return { list: list.filter((e: ISnapshotInfo) => e.id !== data.id) };
+    });
+    setSnapshotLength(snapshotLength - 1);
+  };
+  const onAddMemo = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setEditingMemo(true);
+  };
 
   const onClickSnapshot = () => {
-    sendRequest();
+    sendGet();
   };
 
   return (
@@ -29,12 +57,24 @@ const SnapshotInfo = ({ data }: PropType) => {
       style={{ background: snapshotDetail.id == data.id ? "#29303b" : "" }}
       onClick={onClickSnapshot}
     >
-      <TimeHolder>{data.createdDate}</TimeHolder>
-      <MemoHolder onClick={onAddMemo}>
-        {!data.memo && "메모 추가하기"}
-        {/* 이부분 클릭하면 input으로 바뀐다거나.. 하는 코드 추가 */}
-        <i className="fa-solid fa-pen"></i>
-      </MemoHolder>
+      <TimeHolder>
+        {new Date(data.createdDate).toLocaleString()}
+        <IconButton
+          size="xs"
+          width="fit-content"
+          name="x"
+          onClick={onDeleteSnapshot}
+        />
+      </TimeHolder>
+      {editingMemo ? (
+        <InputBox setInput={setMemo} />
+      ) : (
+        <MemoHolder onClick={onAddMemo}>
+          {!data.memo && "메모 추가하기"}
+          {/* 이부분 클릭하면 input으로 바뀐다거나.. 하는 코드 추가 */}
+          <i className="fa-solid fa-pen"></i>
+        </MemoHolder>
+      )}
     </MainDiv>
   );
 };
@@ -50,10 +90,12 @@ hover:bg-[#191f26] ease-linear
 `;
 
 const TimeHolder = tw.div`
+flex justify-between
 text-base font-bold
 `;
 
 const MemoHolder = tw.div`
+w-fit
 text-xs
 flex items-center gap-[6px]
 `;
