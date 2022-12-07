a295a11f203621e45c5a00ab3adba4fad4c438a1
Charmull <527wlals@gmail.com>
2022-12-07 22:23:54 +0900

    [Feat/Chat] 채팅입력창 추가

--- src/components/Chat/ChatBox.tsx
+++ src/components/Chat/ChatBox.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import LabelTab from "@/components/_styled/LabelTab";
 import tw from "tailwind-styled-components";
 import ChatBubble from "./ChatBubble";
+import InputBox from "../_styled/Input";
 
 const ChatBox = () => {
+  const [chatContent, setChatContent] = useState<string>("");
+
   return (
     <>
       <LabelTab label="채팅" />
       <MainDiv>
-        <ChatBubble />
-        <ChatBubble />
-        <ChatBubble />
-        <ChatBubble />
-        <ChatBubble />
-        <ChatBubble mine={true} />
+        <ChatList>
+          <ChatBubble />
+          <ChatBubble />
+          <ChatBubble />
+          <ChatBubble />
+          <ChatBubble />
+          <ChatBubble mine={true} />
+        </ChatList>
+        <InputBox
+          placeholder="채팅을 입력해주세요."
+          setInput={setChatContent}
+        />
       </MainDiv>
     </>
   );
@@ -21,11 +30,25 @@ const ChatBox = () => {
 
 export default ChatBox;
 
+// const MainDiv = tw.div`
+// w-full h-[calc(100%-32px)]
+// overflow-h-auto
+// dark-2
+// rounded-[0_10px_10px_10px]
+// overflow-x-hidden overflow-y-scroll
+// p-[10px]
+// `;
 const MainDiv = tw.div`
 w-full h-[calc(100%-32px)] 
 overflow-h-auto
 dark-2
 rounded-[0_10px_10px_10px]
-overflow-x-hidden overflow-y-scroll
 p-[10px]
+`;
+
+const ChatList = tw.div`
+w-full h-[calc(100%-32px)] 
+overflow-h-auto
+p-[10px]
+overflow-x-hidden overflow-y-scroll
 `;
