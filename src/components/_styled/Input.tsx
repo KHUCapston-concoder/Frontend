bd9b64833d39e5356bc1e57586730e680dd54b21
Charmull <527wlals@gmail.com>
2022-12-07 23:00:43 +0900

    [Feat/Chat] 채팅용 InputBox 구현
    엔터 입력 시 함수 실행(채팅 보내기)되도록 기능 추가된 InputChatBox 구현
    추후 InputBox와 결합 논의 필요

--- src/components/_styled/Input.tsx
+++ src/components/_styled/Input.tsx
@@ -27,7 +27,47 @@ const InputBox = ({
         className={className + " input input-xs mt-[4px]"}
         disabled={disabled}
         ref={inputRef}
-        onChange={() => setInput(inputRef.current? inputRef.current.value : "")}
+        onChange={() =>
+          setInput(inputRef.current ? inputRef.current.value : "")
+        }
+      />
+    </div>
+  );
+};
+
+// 채팅용 InputBox (엔터 입력 이벤트 추가됨, 추후 InputBox와 결합 논의 필요)
+export const InputChatBox = ({
+  setInput,
+  placeholder,
+  className = "",
+  label,
+  disabled = false,
+  enterHandler,
+}: {
+  PropType;
+  enterHandler: Function;
+}) => {
+  const inputRef = useRef(null);
+  return (
+    <div className="my-[2px] w-full px-[12px] py-[4px]">
+      <div className="w-full text-left text-xs font-bold">
+        {label != undefined && `· ${label}`}
+      </div>
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={className + " input input-xs mt-[4px]"}
+        disabled={disabled}
+        ref={inputRef}
+        onChange={() =>
+          setInput(inputRef.current ? inputRef.current.value : "")
+        }
+        onKeyPress={() => {
+          if (event.keyCode == 13) {
+            event?.preventDefault();
+            enterHandler();
+          }
+        }}
       />
     </div>
   );
