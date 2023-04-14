94eddf37df04fd29ac4979cb43f5f99fffdcba97
wjdwl002 <wjdwl001@khu.ac.kr>
2023-04-14 07:27:28 -0700

    lint

--- src/components/_router/router.tsx
+++ src/components/_router/router.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "@/pages/home";
+import Home from "@/pages/Home";
 import Workspace from "@/pages/workspace/:id";
 import PrivateRouter from "./PrivateRouter";
 import Page404 from "@/pages/404";
